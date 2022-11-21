dd75abe4a61c68ea61ef027299005a8fe96e9f44
Moser Damasceno <moser.damasceno@gmail.com>
2022-11-20 23:53:07 -0300

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,52 @@
 /**
- * @type {import('gatsby').GatsbyConfig}
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
-module.exports = {
+
+// support for .env, .env.development, and .env.production
+require("dotenv").config()
+require("dotenv").config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
+ module.exports = {
   siteMetadata: {
-    siteUrl: `https://www.yourdomain.tld`,
+    title: process.env.SITE_TITLE,
+    description: process.env.SITE_DESCRIPTION,
+    author: process.env.SITE_AUTHOR,
   },
-  plugins: [],
+  plugins: [
+    `gatsby-plugin-image`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    {
+      resolve: `gatsby-plugin-sass`,
+      options: {
+        sassOptions: {
+          includePaths: ["styles"]        }
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: process.env.OPTION_ICON,
+
+      },
+    },
+  ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,176 +1,206 @@
-import * as React from "react"
+import React from "react"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import { StaticImage } from "gatsby-plugin-image"
 
-const pageStyles = {
-  color: "#232129",
-  padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-const headingAccentStyles = {
-  color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
-const listStyles = {
-  marginBottom: 96,
-  paddingLeft: 0,
-}
-const listItemStyles = {
-  fontWeight: 300,
-  fontSize: 24,
-  maxWidth: 560,
-  marginBottom: 30,
-}
+import HubspotForm from "../components/hubspot-form"
 
-const linkStyle = {
-  color: "#8954A8",
-  fontWeight: "bold",
-  fontSize: 16,
-  verticalAlign: "5%",
-}
 
-const docLinkStyle = {
-  ...linkStyle,
-  listStyleType: "none",
-  marginBottom: 24,
-}
+import "../styles/pages/index.scss"
 
-const descriptionStyle = {
-  color: "#232129",
-  fontSize: 14,
-  marginTop: 10,
-  marginBottom: 0,
-  lineHeight: 1.25,
-}
+export default function Home() {
 
-const docLink = {
-  text: "Documentation",
-  url: "https://www.gatsbyjs.com/docs/",
-  color: "#8954A8",
-}
-
-const badgeStyle = {
-  color: "#fff",
-  backgroundColor: "#088413",
-  border: "1px solid #088413",
-  fontSize: 11,
-  fontWeight: "bold",
-  letterSpacing: 1,
-  borderRadius: 4,
-  padding: "4px 6px",
-  display: "inline-block",
-  position: "relative",
-  top: -2,
-  marginLeft: 10,
-  lineHeight: 1,
-}
-
-const links = [
-  {
-    text: "Tutorial",
-    url: "https://www.gatsbyjs.com/docs/tutorial/",
-    description:
-      "A great place to get started if you're new to web development. Designed to guide you through setting up your first Gatsby site.",
-    color: "#E95800",
-  },
-  {
-    text: "How to Guides",
-    url: "https://www.gatsbyjs.com/docs/how-to/",
-    description:
-      "Practical step-by-step guides to help you achieve a specific goal. Most useful when you're trying to get something done.",
-    color: "#1099A8",
-  },
-  {
-    text: "Reference Guides",
-    url: "https://www.gatsbyjs.com/docs/reference/",
-    description:
-      "Nitty-gritty technical descriptions of how Gatsby works. Most useful when you need detailed information about Gatsby's APIs.",
-    color: "#BC027F",
-  },
-  {
-    text: "Conceptual Guides",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
-    description:
-      "Big-picture explanations of higher-level Gatsby concepts. Most useful for building understanding of a particular topic.",
-    color: "#0D96F2",
-  },
-  {
-    text: "Plugin Library",
-    url: "https://www.gatsbyjs.com/plugins",
-    description:
-      "Add functionality and customize your Gatsby site or app with thousands of plugins built by our amazing developer community.",
-    color: "#8EB814",
-  },
-  {
-    text: "Build and Host",
-    url: "https://www.gatsbyjs.com/cloud",
-    badge: true,
-    description:
-      "Now you’re ready to show the world! Give your Gatsby site superpowers: Build and host on Gatsby Cloud. Get started for free!",
-    color: "#663399",
-  },
-]
-
-const IndexPage = () => {
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! 🎉🎉🎉</span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time. 😎
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {link.badge && (
-                <span style={badgeStyle} aria-label="New Badge">
-                  NEW!
+    <Layout>
+      <Seo title="Home" />
+
+      <section className="container" id="banner">
+        <div className="grid-container">
+          <div className="grid">
+            <div className="col content-banner">
+              <div className="img-mobile">
+              <StaticImage src="../images/bluefriday-2.png" alt="Blue Friday"></StaticImage>
+              </div>
+              <h1>Quer aproveitar a Black Friday <br/>mas está no <span>vermelho</span>?</h1>
+              <p>
+                Aproveite as melhores promoções do ano <br/>
+                <span>sem usar os limites do cartão nem o 13⁰.</span> </p>
+                <p><b>Dinheiro fácil, rápido e sem burocracia!</b></p>
+            </div>
+            <div className="col">
+              <div className="form-hubspot">
+                <div className="header-form">
+                  <h2>Simule agora</h2>
+                </div>
+                <HubspotForm></HubspotForm>
+                <p>Lorem ipsum may be used as a placeholder before final copy is available.</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="dinheiro-na-conta" className="container">
+        <div className="grid-container">
+          <div className="grid">
+            <div className="col">
+              <h2>Dinheiro na conta AGORA e a primeira <br/>parcela só pro ANO QUE VEM!</h2>
+            </div>
+          </div>
+          <div className="grid grid-2">
+            <div className="col">
+              <div className="image">
+                <StaticImage src="../images/img-sem-juros.png" alt="Sem juros"></StaticImage>
+              </div>
+              <div className="content">
+                <h3>Sem juros</h3>
+                <p>Até <b>R$ 1.000,00 SEM JUROS</b> <br/>de verdade!</p>
+              </div>
+            </div>
+            <div className="col">
+              <div className="image">
+                <StaticImage src="../images/img-calendario.png" alt="calendario"></StaticImage>
+              </div>
+              <div className="content">
+                <h3>SÓ PAGA ANO <br/>QUE VEM</h3>
+                <p>Primeira parcela <b>só em 2023!</b></p>
+              </div>
+            </div>
+            <div className="col">
+              <div className="image">
+                <StaticImage src="../images/img-dinheiro-rapido.png" alt="dinheiro rapido"></StaticImage>
+              </div>
+              <div className="content">
+                <h3>DINHEIRO RÁPIDO</h3>
+                <p>Dinheiro na conta em <b>até 24hs</b><br/> depois de aprovado!</p>
+              </div>
+            </div>
+            <div className="col">
+              <div className="image">
+                <StaticImage src="../images/img-credito.png" alt="credito"></StaticImage>
+              </div>
+              <div className="content">
+                <h3>CRÉDITO <br/>PRÉ-APROVADO</h3>
+                <p><b>Sem consulta</b> ao SPC e Serasa!</p>
+              </div>
+            </div>
+          </div>
+          <div className="grid">
+            <div className="col">
+              <p>Vai perder essa?</p>
+              <a href="#" className="btn btn-azul">Fale Conosco</a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="produtos" className="container">
+        <div className="grid-container">
+          <div className="grid">
+            <div className="col">
+              <h2>Compre AGORA sem usar seu limite do cartão! <br/>RÁPIDO, FÁCIL e EXCLUSIVO!</h2>
+              <p>O dinheiro cai na sua conta em até 24 horas depois de aprovado e você <br/>compra o que quiser e onde desejar!</p>
+            </div>
+          </div>
+          <div className="grid grid-produtos">
+            <div className="col">
+              <div className="images-produtos">
+                <StaticImage src="../images/img-produto-01.jpg" alt="Produto 01"></StaticImage>
+                <StaticImage src="../images/img-produto-01.jpg" alt="Produto 01"></StaticImage>
+                <StaticImage src="../images/img-produto-01.jpg" alt="Produto 01"></StaticImage>
+              </div>
+              <p><b>R$ 500</b> em <b>6x SEM JUROS</b>, de verdade</p>
+            </div>
+            <div className="col">
+              <div className="images-produtos">
+                <StaticImage src="../images/img-produto-02.jpg" alt="Produto 02"></StaticImage>
+                <StaticImage src="../images/img-produto-02.jpg" alt="Produto 02"></StaticImage>
+                <StaticImage src="../images/img-produto-02.jpg" alt="Produto 02"></StaticImage>
+              </div>
+              <p><b>R$ 1.000</b> em <b>3x SEM JUROS</b>, de verdade <br/>ou <b>10x</b> com <b>taxa de 0,99%</b> </p>
+            </div>
+          </div>
+          <div className="grid">
+            <div className="col">
+              <a href="#" className="btn btn-branco">Eu quero!</a>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section id="liberdade-financeira" className="container">
+        <div className="grid-container">
+          <div className="grid">
+            <div className="col">
+              <h2>
+                Liberdade financeira de verdade! <br/>
+                Condições imperdíveis para você aproveitar a Black <br/>Friday do seu jeito, onde e como quiser
+              </h2>
+            </div>
+          </div>
+          <div className="grid grid-3">
+            <div className="col">
+              <div className="box-preco">
+                <span className="preco">
+                  R$ 500
                 </span>
-              )}
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
-      <img
-        alt="Gatsby G Logo"
-        src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
-      />
-    </main>
-  )
-}
+                <hr />
+                <span className="condicao">
+                  6x SEM JUROS
+                </span>
+                <span className="textinho">
+                  de verdade
+                </span>
+              </div>
+            </div>
+            <div className="col">
+            <div className="box-preco">
+                <span className="preco">
+                  R$ 1.000
+                </span>
+                <hr />
+                <span className="condicao">
+                  3x SEM JUROS
+                </span>
+                <span className="textinho">
+                  de verdade
+                </span>
+              </div>
+            </div>
+            <div className="col">
+            <div className="box-preco">
+                <span className="preco">
+                  R$ 1.000
+                </span>
+                <hr />
+                <span className="condicao">
+                  10x COM TAXA DE <span>0,99%</span>
+                </span>
+              </div>
+            </div>
+          </div>
+          <div className="grid">
+            <div className="col">
+              <a href="#" className="btn btn-verde">Solicite agora</a>
+            </div>
+          </div>
+        </div>
+      </section>
 
-export default IndexPage
+      <footer id="footer" className="container">
+        <div className="grid-container">
+          <div className="grid">
+            <div className="col">
+              <div className="logo-footer">
+                <StaticImage src="../images/logo-footer.png" alt="Logo footer"></StaticImage>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
 
-export const Head = () => <title>Home Page</title>
+    
+    </Layout>
+    )
+  }
+  
